d86c26378b4486216c5cb0fcace9e2fba85e0dde
vnstsn <va.tsang@yahoo.fr>
2021-10-14 12:07:20 +0200

    first step, init Pokemon

--- src/code/Pokemon.java
+++ src/code/Pokemon.java
@@ -1,5 +1,37 @@
 package code;
 
 public class Pokemon {
+	private String name;
+	private int hp;
+	private int atk;
+	
+	public Pokemon() {
+		
+	}
+	
+	public Pokemon(String name, int hp, int atk) {
+		this.name = name;
+		this.hp = hp;
+		this.atk = atk;
+	}
+	
+	
+	public boolean isDead() {
+		if(hp == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+//	public void attackPokemon() {
+//		(this.hp - this.atk);
+//	}
 
+	@Override
+	public String toString() {
+		return "Pokemon [name=" + name + ", hp=" + hp + ", atk=" + atk + "]";
+	}
+	
+	
 }
